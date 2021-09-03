517664ce98822381118f95bd431f037f00688fc4
DashaGKH <keshonti2016@gmail.com>
2021-09-03 13:33:03 -0400

    replace odd fizz/fizbuzz V

---
+++ fizbuzz.js
@@ -0,0 +1,28 @@
+// function rep(arr){
+//   for(let i = 0; i< arr.length; i++){
+//     if(arr[i] % 15 === 0){
+//       arr.splice(arr.indexOf(arr[i]),1, 'fizzbuzz')
+//     } else if(arr[i] % 10 === 0){
+//       arr.splice(arr.indexOf(arr[i]),1, 'buzz')
+//     }else if(arr[i] % 5 === 0){
+//       arr.splice(arr.indexOf(arr[i]),1, 'fizz')
+//     }
+//   }return arr
+// }
+// console.log(rep([4, 6, 5, 15, 20, 30, 150]));
+
+
+
+
+// function replace(arr){
+//   let a = 'fizz'
+//   let b = 'buzz'
+//   let c = 'fizbuzz'
+
+// for(let i = 0; i < arr.length; i++){
+// if(arr[i] % 15 === 0)arr[i] = c
+// if(arr[i] % 10 === 0)arr[i] = b
+// if(arr[i] % 5 === 0)arr[i] = a
+// }return arr
+// }
+// console.log(replace([4, 6, 5, 15, 20, 30, 150]))
