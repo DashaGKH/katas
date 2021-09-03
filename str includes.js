8aeceba048666272defbe4f959db4521c1e95339
DashaGKH <keshonti2016@gmail.com>
2021-09-03 13:46:22 -0400

    check if the string contains vowels

---
+++ str includes.js
@@ -0,0 +1,13 @@
+// does this string includes vowels?
+
+
+// function vow(str){
+//   let str1 = ''
+//   for(let i = 0; i < str.length;i++){
+//     if('aeouiy'.includes(str[i])){
+//         str1 = str1 + str[i]
+//         }
+//   }return str1
+// }
+
+// console.log(vow('byoba'));
