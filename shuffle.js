47b76c3adbb3c5c8e21bbf4aa3078b9fa7933de3
DashaGKH <keshonti2016@gmail.com>
2021-09-03 13:41:29 -0400

    Shuffle/same/differets length

---
+++ shuffle.js
@@ -0,0 +1,42 @@
+// // Two strings given:
+// // shuffle a - lower case,
+// // b - upper case from back to beginning, same length
+
+
+// function shuffle(a, b){
+//   let arr = []
+//   let b1 = ''
+//   for(let i = b.length -1; i >= 0; i--){
+//     b1 = b1 + b[i]
+//   }
+//   for(let y = 0; y < b1.length; y++){
+//     arr.push(a[y].toLowerCase(), b1[y].toUpperCase())
+//   }return arr
+//   }
+
+// console.log(shuffle('Bfbp','EmuI'));
+
+
+
+
+
+
+// Two strings, modified task
+// shuffle a - lower case,
+// b - upper case from back to beginning, different length
+
+// function shuffle(a, b){
+//   let max;
+//   let arr = []
+//   let b1 = ''
+//   for(let i = b.length -1; i >= 0; i--){
+//     b1 = b1 + b[i]
+//   }
+
+//   max =  a.length > b1.length ? a.length :  b.length
+//    for(let y = 0; y < max; y++){
+//     arr.push(a[y].toLowerCase(), b1[y].toUpperCase())
+//   }return arr
+//   }
+
+// console.log(shuffle('Bfbp','EmuI'));
