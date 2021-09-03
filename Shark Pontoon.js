135bdf798b4441619e7c6eebe21bc701f2398024
DashaGKH <keshonti2016@gmail.com>
2021-09-03 13:37:29 -0400

    Shark ponton Codewars

---
+++ Shark Pontoon.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/57e921d8b36340f1fd000059
+// 8 kata
+
+
+// function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+//     if(dolphin === true){
+//         sharkSpeed = sharkSpeed / 2
+//     }
+//     let yourTime = pontoonDistance / youSpeed
+//     let sharkTime = sharkDistance / sharkSpeed
+//     if(yourTime <  sharkTime){
+//         return 'Alive!'
+//     } return 'Shark Bait!'
+// }
+
+
