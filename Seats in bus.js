f53e8bb171eac7f73a821e37a126c262df0f323c
DashaGKH <keshonti2016@gmail.com>
2021-09-04 23:09:11 -0400

    Bus, seats, count how many will wait

---
+++ Seats in bus.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5875b200d520904a04000003
+// how many ppl will wait
+
+
+// function enough(cap, on, wait) {
+//     let seats = cap - on
+//     if(cap - (on + wait) >= 0){
+//         return 0
+//     }
+//     else if(seats < wait ) {
+//         return (wait - seats)
+//     }
+// }
