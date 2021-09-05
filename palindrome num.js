e3a8a401677c8743a0f298039adb26c0bf827b32
DashaGKH <keshonti2016@gmail.com>
2021-09-04 23:04:39 -0400

    Two numbers given, print each number from min to max

---
+++ palindrome num.js
@@ -0,0 +1,12 @@
+// function isPalindrome(num){
+// let str1 = ''
+// num = num + ''
+// for(let i = num.length - 1; i >= 0; i--){
+// str1 = str1 + num[i]
+// }
+//   if(str1 === num){
+// return 'palindrome'
+// } return 'not a palindrome'
+// }
+
+// console.log(isPalindrome(656));
---
+++ print each from min to max.js
@@ -0,0 +1,24 @@
+// given two numbers, one is bigger then other
+// print all nums from min to max
+// - check which is min/max
+// - loop where i is min and i < max
+// create empty arr, return push in that array
+//
+
+// function num(a, b){
+//   let arr=[]
+//   let min = a < b ? a  : b
+//   let max = a === min ? b : a
+//   for(let i = min; i <= max ; i++){
+//     arr.push(i)
+// } return arr
+// }
+
+// console.log(num(55,22));
+
+
+// errors
+// i put arr.push[i] insted arr.push(i)
+// i cant push arr[i] becouse this element
+// doesnt even exist yet. (i) is the number in the loop
+
