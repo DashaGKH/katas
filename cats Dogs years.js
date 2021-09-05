437ecb8f1d4bbc7df46867f356dc6e904675cdee
DashaGKH <keshonti2016@gmail.com>
2021-09-04 23:11:00 -0400

    Cats Dogs years

---
+++ cats Dogs years.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b
+
+
+
+// var humanYearsCatYearsDogYears = function(humanYears) {
+//     let catY = 15
+//     let dogY = 15
+//     if(humanYears === 1)
+//         return [humanYears, catY, dogY]
+//     if(humanYears === 2){
+//         return [humanYears, catY += 9, dogY += 9]
+//     } if(humanYears > 2){
+//         return [humanYears, ((humanYears - 2)* 4)+ 24,((humanYears - 2)* 5)+ 24]
+//     }
+// }
