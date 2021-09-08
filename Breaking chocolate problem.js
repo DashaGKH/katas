72c465aa72678d3092eccae89dec47840b9e9a38
DashaGKH <keshonti2016@gmail.com>
2021-09-08 18:30:00 -0400

    Breaking chocolate problem

---
+++ Breaking chocolate problem.js
@@ -0,0 +1,12 @@
+// function breakChocolate(n,m) {
+//     let breaks = 0;
+//     let square = n * m;
+//     while (square > 1){
+//         --square;
+//         ++breaks;
+//     }
+//     return breaks;
+// }
+
+
+//https://www.codewars.com/kata/534ea96ebb17181947000ada
