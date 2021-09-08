2ade545935a4a4655afacf1f2ff40c08b76a33fe
DashaGKH <keshonti2016@gmail.com>
2021-09-07 22:38:39 -0400

    I love you, a little , a lot, passionately

---
+++ I love you, a little , a lot, passionately ... not at all.js
@@ -0,0 +1,16 @@
+// // https://www.codewars.com/kata/57f24e6a18e9fad8eb000296
+//
+//
+// function howMuchILoveYou(nbPetals) {
+//     let phrase = [
+//         'not at all',
+//         'I love you',
+//         'a little',
+//         'a lot',
+//         'passionately',
+//         'madly',
+//         'not at all'
+//     ]
+//     return phrase[nbPetals % 6]
+// }
+
