23d1c74cc4171162abad77a7a066f69af1e8a568
DashaGKH <keshonti2016@gmail.com>
2021-09-09 13:47:10 -0400

    New Value for var

---
+++ New value for variables.js
@@ -0,0 +1,10 @@
+// function newValues(a, b){
+//     let c = a
+//     if(a === b){
+//         return [a = 0 , b = 0]
+//     } if (a !== b) {
+//         a = (a + b)
+//         b = (c - b)
+//     }return [a , b]
+// }
+
