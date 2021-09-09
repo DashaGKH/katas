afd2ee54b2f16724ac812f64f52e15ae7d016bcf
DashaGKH <keshonti2016@gmail.com>
2021-09-09 16:46:03 -0400

    debugging 101 dalmatinas

--- New value for variables.js
+++ New value for variables.js
@@ -1,10 +1,2 @@
-// function newValues(a, b){
-//     let c = a
-//     if(a === b){
-//         return [a = 0 , b = 0]
-//     } if (a !== b) {
-//         a = (a + b)
-//         b = (c - b)
-//     }return [a , b]
-// }
+
 
